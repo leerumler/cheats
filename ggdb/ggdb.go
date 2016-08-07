966ba3164884a357b43225d7b9ba15c2e17e088b
Lee Rumler <lee@howdolinux.com>
2016-08-07 05:03:42 -0400

    add expansion inserts and id lookups

--- ggdb/ggdb.go
+++ ggdb/ggdb.go
@@ -10,6 +10,11 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+// expander defines a struct that holds text epansion information.
+type expander struct {
+	phrase, expansion string
+}
+
 func findGGDB() *string {
 
 	// Check the current user.
@@ -37,11 +42,11 @@ func CreateGengarDB() {
 	dbfile := findGGDB()
 
 	// Open the database file.  This should create it if it doesn't exist.
-	ggdb, err := sql.Open("sqlite3", *dbfile)
+	db, err := sql.Open("sqlite3", *dbfile)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer ggdb.Close()
+	defer db.Close()
 
 	createTables := `
 	drop table if exists expansions;
@@ -57,9 +62,57 @@ func CreateGengarDB() {
 	);
 	`
 
-	_, err = ggdb.Exec(createTables)
+	_, err = db.Exec(createTables)
 	if err != nil {
 		log.Printf("%q: %s\n", err, createTables)
 		return
 	}
+}
+
+// InsertExpansion inserts an Expansion into gengar's database.
+func InsertExpansion(exp expander) {
+
+	// Find the database and open it.
+	dbfile := findGGDB()
+	db, err := sql.Open("sqlite3", *dbfile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Insert the expansion.
+	_, err = db.Exec("insert into expansions (expansion) values ($1);", exp.expansion)
+	if err != nil {
+		log.Fatal(err)
+
+	}
+}
+
+func findExpansionID(exp expander) int {
+	// Find the database and open it.
+	dbfile := findGGDB()
+	db, err := sql.Open("sqlite3", *dbfile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	var expID int
+	err = db.QueryRow("select id from expansions where expansion=$1", exp.expansion).Scan(&expID)
+	switch {
+	case err == sql.ErrNoRows:
+		log.Fatal("No matching expansions found.")
+	case err != nil:
+		log.Fatal(err)
+	default:
+		log.Println("Expansion ID is", expID)
+	}
+
+	return expID
+
+}
+
+// MapPhrase maps a phrase to an expansion in gengar's database.
+func MapPhrase(exp expander) {
+
 }
