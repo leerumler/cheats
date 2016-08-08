ad6b7ffc175ca8131ba8bebe60c4d00b0c155f1e
Lee Rumler <lee@howdolinux.com>
2016-08-08 06:46:06 -0400

    add basic view

--- ggui/ggui.go
+++ ggui/ggui.go
@@ -7,9 +7,18 @@ import (
 	"github.com/jroimartin/gocui"
 )
 
-func layout(g *gocui.Gui) error {
-	maxX, maxY := g.Size()
-	if v, err := g.SetView("hello", maxX/2-7, maxY/2, maxX/2+7, maxY/2+2); err != nil {
+// GetGooey creates the GUI.
+func GetGooey() {
+	gooey := gocui.NewGui()
+	if err := gooey.Init(); err != nil {
+		// handle error
+	}
+	defer gooey.Close()
+}
+
+func layout(gooey *gocui.Gui) error {
+	maxX, maxY := gooey.Size()
+	if v, err := gooey.SetView("hello", 0, 0, maxX-1, maxY-1); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
@@ -22,20 +31,21 @@ func quit(g *gocui.Gui, v *gocui.View) error {
 	return gocui.ErrQuit
 }
 
-func example() {
-	g := gocui.NewGui()
-	if err := g.Init(); err != nil {
+// Example creates an example GUI.
+func Example() {
+	gooey := gocui.NewGui()
+	if err := gooey.Init(); err != nil {
+		log.Fatal(err)
+	}
+	defer gooey.Close()
+
+	gooey.SetLayout(layout)
+
+	if err := gooey.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
 		log.Panicln(err)
 	}
-	defer g.Close()
 
-	g.SetLayout(layout)
-
-	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
-		log.Panicln(err)
-	}
-
-	if err := g.MainLoop(); err != nil && err != gocui.ErrQuit {
+	if err := gooey.MainLoop(); err != nil && err != gocui.ErrQuit {
 		log.Panicln(err)
 	}
 }
