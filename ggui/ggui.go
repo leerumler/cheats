48683c084e02f890c0598ea03ceb49376d0e4a68
Lee Rumler <lee@howdolinux.com>
2016-08-10 18:19:48 -0400

    flesh out mapPhrases layout

--- ggui/ggui.go
+++ ggui/ggui.go
@@ -22,13 +22,59 @@ func getGooey() {
 	defer gooey.Close()
 }
 
+func centerText(text string, maxX int) string {
+	numSpaces := maxX/2 - len(text)/2
+	for i := 0; i < numSpaces; i++ {
+		text = " " + text
+	}
+	return text
+}
+
+func padText(text string, maxX int) string {
+	numSpaces := maxX - len(text)
+	for i := 0; i < numSpaces; i++ {
+		text = text + " "
+	}
+	return text
+}
+
+func drawHeader(gooey *gocui.Gui) error {
+
+	maxX, _ := gooey.Size()
+
+	if header, err := gooey.SetView("header", 0, 0, maxX, 2); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+
+		head := centerText("Gengar Configuration Editor", maxX)
+
+		fmt.Fprintln(header, head)
+	}
+
+	return nil
+
+}
+
 func drawPhrases(gooey *gocui.Gui) error {
 
 	// Get window size.
 	maxX, maxY := gooey.Size()
 
+	_, _, _, minY, err := gooey.ViewPosition("header")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if phraseHead, err := gooey.SetView("phraseHead", 0, minY, maxX/6, minY+2); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+		fmt.Fprintln(phraseHead, "Phrases")
+	}
+
 	//
-	if phraseView, err := gooey.SetView("phrases", 0, 0, maxX-1, maxY-1); err != nil {
+	if phraseView, err := gooey.SetView("phrases", 0, minY+2, maxX/6, maxY-1); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
@@ -37,7 +83,8 @@ func drawPhrases(gooey *gocui.Gui) error {
 		phraseView.Highlight = true
 
 		for _, exp := range *exps {
-			fmt.Fprintln(phraseView, exp.Phrase)
+			phrase := padText(exp.Phrase, maxX)
+			fmt.Fprintln(phraseView, phrase)
 		}
 	}
 
@@ -50,14 +97,39 @@ func drawExpansions(gooey *gocui.Gui) error {
 	// Get window size.
 	maxX, maxY := gooey.Size()
 
-	if expView, err := gooey.SetView("galaxy", 10, 0, maxX-1, maxY-1); err != nil {
+	_, _, _, minY, err := gooey.ViewPosition("header")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, _, minX, _, err := gooey.ViewPosition("phrases")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if expHead, err := gooey.SetView("expHead", minX, minY, maxX-1, minY+2); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
+		fmt.Fprintln(expHead, "Expansions")
+	}
+
+	if expView, err := gooey.SetView("galaxy", minX, minY+2, maxX-1, maxY-1); err != nil {
+		if err != gocui.ErrUnknownView {
+			return err
+		}
+
 		fmt.Fprintln(expView, "Long ago, in a galaxy far, far away, there was no need for overly complicated programming languages designed to make simple tasks easier for users and more difficult for programmers.  In fact, there were no users, programmers, or computers.  There was nothing, in fact, because the galaxy did not contain life.")
 		expView.Wrap = true
 	}
 
+	return nil
+}
+
+func mapPhrases(gooey *gocui.Gui) error {
+	drawHeader(gooey)
+	drawPhrases(gooey)
+	drawExpansions(gooey)
 	return nil
 }
 
@@ -80,8 +152,7 @@ func GengarMenu() {
 	}
 	defer gooey.Close()
 
-	gooey.SetLayout(drawPhrases)
-	// gooey.SetLayout(drawExpansions)
+	gooey.SetLayout(mapPhrases)
 
 	if err := gooey.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
 		log.Panicln(err)
